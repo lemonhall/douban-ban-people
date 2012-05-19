8c92b889b4083a9ad53e639ec90136efa9fa417c
lemonhall <lemonhall@gmail.com>
2012-05-19 19:14:35 +0800

    get ban people from blacklist

---
+++ contentscript.js
@@ -0,0 +1,9 @@
+﻿/*
+ * Copyright (c) 2010 The Chromium Authors. All rights reserved.  Use of this
+ * source code is governed by a BSD-style license that can be found in the
+ * LICENSE file.
+ */
+(function(){
+	var people=$("a[href*='http://www.douban.com/people/theanna/']");
+	people.parent().parent().parent().hide(); 
+} )();
